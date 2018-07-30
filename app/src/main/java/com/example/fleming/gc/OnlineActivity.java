9723d500be70b1db330844f3ab8cb4e1b2e9b4a2
Fleming <16278383@qq.com>
2018-07-30 20:15:52 +0800

    websocket处理

--- app/src/main/java/com/example/fleming/gc/OnlineActivity.java
+++ app/src/main/java/com/example/fleming/gc/OnlineActivity.java
@@ -6,13 +6,25 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.fleming.request.OnlineRequest;
+import com.example.fleming.request.form.SocketMessage;
+import com.google.gson.Gson;
+import com.pusher.java_websocket.client.WebSocketClient;
+import com.pusher.java_websocket.drafts.Draft_10;
+import com.pusher.java_websocket.handshake.ServerHandshake;
+
+import java.net.URI;
+import java.net.URISyntaxException;
 
 public class OnlineActivity extends AppCompatActivity{
 
     private TextView onlineState;
     private Button goControlB;
+
+    //此webSocket用来向web端传输在线信息、接收对面在线信息、发送下线信息、接受下线信息
+    private WebSocketClient mSocketClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,11 +45,14 @@ public class OnlineActivity extends AppCompatActivity{
             goControlB.setEnabled(true);
 
             goControlB.setOnClickListener(bocl);
+
         }
 
     }
 
-    //用于处理到Web上线时发来的信息：改页面在线标志，改按钮可以点击
+    //----------------------------------------------------------------------------------------------
+
+    //用于处理Web上线时发来的信息：改页面在线标志，改按钮可以点击
     public void onWebOnLineMessage(){
 
         onlineState = findViewById(R.id.OnlineState);
@@ -51,6 +66,21 @@ public class OnlineActivity extends AppCompatActivity{
         goControlB.setOnClickListener(bocl);
     }
 
+    //用于处理Web下线时发来的信息：改页面标志，改按钮不可点击
+    public void onWebOfflineMessage(){
+
+        onlineState = findViewById(R.id.OnlineState);
+        goControlB = findViewById(R.id.goControlB);
+
+        onlineState.setText("未上线");
+        onlineState.setTextColor(Color.parseColor("#666666"));
+
+        goControlB.setEnabled(true);
+
+    }
+
+    //----------------------------------------------------------------------------------------------
+
     //按钮全局点击事件
     public final View.OnClickListener bocl = new View.OnClickListener(){
         @Override
@@ -58,5 +88,70 @@ public class OnlineActivity extends AppCompatActivity{
 
         }
     };
+
+    //----------------------------------------------------------------------------------------------
+
+    private void initWebSocket(final String username) {
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+                URI uri = null;
+
+                try {
+                    uri = new URI("ws://118.25.180.193/SocketHandle/app/"+username);
+                } catch (URISyntaxException e) {
+                    e.printStackTrace();
+                }
+
+                mSocketClient = new WebSocketClient(uri,new Draft_10()) {
+
+                    Gson gson = new Gson();
+
+                    @Override
+                    public void onOpen(ServerHandshake handshakedata) {
+                        Toast.makeText(OnlineActivity.this,"连接成功",Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onMessage(String message) {
+
+                        SocketMessage socketMessage = gson.fromJson(message, SocketMessage.class);
+
+                        //能收到两种消息：对面上线，对面下线
+                        if ("webIsOnline".equals(socketMessage.getMessageType())){
+                            onWebOnLineMessage();
+                        } else if ("webIsOffline".equals(socketMessage.getMessageType())) {
+                            onWebOfflineMessage();
+                        }
+
+                    }
+
+                    @Override
+                    public void onClose(int code, String reason, boolean remote) {
+
+                        SocketMessage socketMessage = new SocketMessage();
+
+                        //连接关闭时发送：我下线了
+                        socketMessage.setMessageType("AppIsOffline");
+                        socketMessage.setData("");
+                        String json = gson.toJson(socketMessage);
+
+                        if(mSocketClient != null){
+                            mSocketClient.send(json);
+                        }
+
+                    }
+
+                    @Override
+                    public void onError(Exception ex) {
+                        Toast.makeText(OnlineActivity.this,"连接错误",Toast.LENGTH_SHORT).show();
+                    }
+                };
+
+            }
+        });
+    }
 
 }
---
+++ app/src/main/java/com/example/fleming/request/form/SocketMessage.java
@@ -0,0 +1,23 @@
+package com.example.fleming.request.form;
+
+public class SocketMessage {
+
+    private String messageType;
+    private String data;
+
+    public String getMessageType() {
+        return messageType;
+    }
+
+    public void setMessageType(String messageType) {
+        this.messageType = messageType;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+}
